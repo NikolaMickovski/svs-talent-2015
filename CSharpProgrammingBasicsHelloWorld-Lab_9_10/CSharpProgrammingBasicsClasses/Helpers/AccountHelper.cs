e996bb8e270df77b61b95ff2c388bf4fa303be36
Nikola Mickovski <nikolamickovski@yahoo.com>
2015-04-06 15:57:46 +0200

    3.57

--- CSharpProgrammingBasicsHelloWorld-Lab_9_10/CSharpProgrammingBasicsClasses/Helpers/AccountHelper.cs
+++ CSharpProgrammingBasicsHelloWorld-Lab_9_10/CSharpProgrammingBasicsClasses/Helpers/AccountHelper.cs
@@ -41,5 +41,12 @@ namespace CSharpProgrammingBasicsClasses
         {
             s_Account_ID = 0;
         }
+
+
+        private string GetAccountNumber(long accountID)
+        {
+            string tmp = Convert.ToString(accountID);
+            int dolzina_na_ID = tmp.Length;                    
+        }
     }
 }
