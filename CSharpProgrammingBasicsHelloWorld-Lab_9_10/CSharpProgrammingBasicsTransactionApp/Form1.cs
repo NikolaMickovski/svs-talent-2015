b3b8c369362debf874cb2c35b785726ac533b7e6
NikolaMickovski <nikolamickovski@yahoo.com>
2015-04-07 09:54:43 +0200

    9.54am

--- CSharpProgrammingBasicsHelloWorld-Lab_9_10/CSharpProgrammingBasicsTransactionApp/Form1.cs
+++ CSharpProgrammingBasicsHelloWorld-Lab_9_10/CSharpProgrammingBasicsTransactionApp/Form1.cs
@@ -143,10 +143,10 @@ namespace CSharpProgrammingBasicsTransactionApp
             ITransactionProcessor tp = new TransactionProcessor();
             
             
-          //  tp.ProcessTransaction(TransactionType.Transfer, new CurrencyAmount(20000, "MKD"), ta, da);
-         //   PopulateAccounts(ta, da);
-            tp.ProcessTransaction(TransactionType.Transfer, new CurrencyAmount(20000, "MKD"), da,la);
-            PopulateAccounts(da, la);
+            tp.ProcessTransaction(TransactionType.Transfer, new CurrencyAmount(20000, "MKD"), ta, da);
+            PopulateAccounts(ta, da);
+           // tp.ProcessTransaction(TransactionType.Transfer, new CurrencyAmount(20000, "MKD"), da,la);
+           // PopulateAccounts(da, la);
         }
         /// <summary>
         /// Prikaz na dve smetki
