14451d1198e51e9364011a2bb9344dec3dc3835e
Nikola Mickovski <nikolamickovski@yahoo.com>
2015-04-15 14:58:25 +0200

    Lab-SOLID
    Exercise SOLID

---
+++ SOLID-Lab/Labs/01.03 SOLID_ISP/ExampleIsp/ExampleIsp/TheGood/ICarFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleIsp.TheGood
+{
+    /// <summary>
+    /// Ovoj interface gi ima site metodi koi gi ima vo IFactory
+    /// </summary>
+    public interface ICarFactory : IFactory
+    {
+        int ProduceBody();
+
+        int ProduceEngine();
+
+        int ProduceGears();
+
+        int ProduceHeadLamps();
+
+        int ProduceElectronics();
+
+        /// <summary>
+        /// Produce tanks for the car
+        /// </summary>
+        /// <returns></returns>
+        int ProduceTank();
+
+        //the painters
+        int Paint(int paintColour);
+    }
+}
---
+++ SOLID-Lab/Labs/01.03 SOLID_ISP/ExampleIsp/ExampleIsp/TheGood/ProduceAllFactory.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleIsp.TheGood
+{
+    public class ProduceAllFactory : IFactory, IMotorBikeFactory, ICarFactory, IBikeFactory
+    {
+        //koi metodi treba da se implementiraat, a koi Implicitno da se implementiraat?
+        public int Paint(int paintColour)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PaintBike(int paintColour)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PaintMoto(int paintColour)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceBody()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceElectronics()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceEngine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceEngineMoto()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceFrame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceFrameBike()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceGears()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceGearsBike()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceGearsMoto()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceHandles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceHandlesBike()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceHeadLamps()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceTank()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProduceTankMoto()
+        {
+            throw new NotImplementedException();
+        }
+
+        int IFactory.CloseShift()
+        {
+            throw new NotImplementedException();
+        }
+
+        int IFactory.StartProductionLane()
+        {
+            throw new NotImplementedException();
+        }
+
+        int IFactory.StartShift()
+        {
+            throw new NotImplementedException();
+        }
+
+        int IFactory.StopProductionLane()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
